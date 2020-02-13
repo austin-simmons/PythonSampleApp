7d6bbb084ae7c9cb3e8b05c0fd47a1c3f6dd7f47
Austin Simmons <autto91@gmail.com>
2020-02-13 10:38:58 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>It Works!</h1>"
+
+
+@app.route("/hello/<name>")
+def get_name(name):
+    return f"<h1>Hello {name}!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=False)
